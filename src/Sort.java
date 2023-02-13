bfa619083362ca06f1b3e7d907742dd62e94d9b3
LanluZ <41728500+LanluZ@users.noreply.github.com>
2023-02-13 20:57:55 +0800

    Update Sort.java

--- src/Sort.java
+++ src/Sort.java
@@ -1,38 +1,34 @@
+import java.net.IDN;
+
 class Sort {
+    public static int[] merge(int[] a){
+        int l = a.length / 2;
+        int r = a.length - l;
 
-    protected static int[] merge(int[] a) {
-        int l = a.length;
-        //拆分到最小组
-        if (a.length < 2)
-            return a;
-        //拆分
-        int[] left = new int[l/2];
-        int[] right = new int[l-l/2];
-        //这玩意比copyOfRange好用多了
-        System.arraycopy(a, 0, left, 0, l / 2);
-        System.arraycopy(a, l / 2, right, 0, l - l / 2);
+        int[] left = new int[l];
+        int[] right = new int[r];
 
-        //排序
-        return sort(merge(left), merge(right));
+        System.arraycopy(a,0,left,0,l);
+        System.arraycopy(a,l,right,0,r);
+
+        if(a.length < 2)return a;
+        return sort(merge(left),merge(right));
     }
 
-    private static int[] sort(int[] left, int[] right) {
-        //索引
-        int index = 0, r = 0, l = 0;
-        //返回数组
-        int[] result = new int[left.length+right.length];
+    public static int[] sort(int[] left,int[] right){
+        int[] result = new int[left.length + right.length];
+        int r = 0,l = 0,index = 0;
 
         while (index < result.length) {
-            if (r >= right.length) {
-                result[index] = left[l++];
-            } else if (l >= left.length) {
-                result[index] = right[r];
+            if (l >= left.length) {
+                result[index++] = right[r++];
+            } else if (r >= right.length) {
+                result[index++] = left[l++];
+            } else if (left[l] > right[r]) {
+                result[index++] = right[r++];
             } else if (left[l] <= right[r]) {
-                result[index] = left[l++];
-            } else if (right[r] < left[l]) {
-                result[index] = right[r++];
+                result[index++] = left[l++];
             }
-            index++;
         }
         return result;
     }
